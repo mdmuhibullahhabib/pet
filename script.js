807a3a20c19095d69f46dc65fad99b3e01d3de67
mdmuhibullahhabib <mdmuhibullahhabib6348@gmail.com>
2024-10-10 00:10:44 +0600

    commit liked button

---
+++ script.js
@@ -0,0 +1,194 @@
+
+// load Catagories
+const loadCatagories = () => {
+  fetch('https://openapi.programming-hero.com/api/peddy/categories')
+    .then((res) => res.json())
+    .then((data) => displayCatagories(data.categories));
+}
+
+// load pets
+const loadPets = () => {
+  fetch('https://openapi.programming-hero.com/api/peddy/pets')
+    .then((res) => res.json())
+    .then((data) => displayPets(data.pets));
+}
+
+// remove active class
+const removeActiveClass = () => {
+  const buttons = document.getElementsByClassName("remove-btn");
+  console.log(buttons)
+  for (let btn of buttons) {
+    btn.classList.remove("active");
+  }
+}
+
+// load  Category Pets
+const loadCategoryPets = (category) => {
+  fetch(`https://openapi.programming-hero.com/api/peddy/category/${category}`)
+    .then((res) => res.json())
+    .then((data) => {
+      // remove active class
+      removeActiveClass();
+
+      // Add active class
+      const activeBtn = document.getElementById(`btn-${category}`);
+      activeBtn.classList.add("active");
+      displayPets(data.data)
+    });
+    
+}
+
+// spinner
+const loadCategoryPetsSpinner =() => {
+  document.getElementById("spinner").style.display = "block";
+
+  setTimeout( function () {
+    loadCategoryPets()
+  },2000)
+}
+
+// Load Details
+const loadDetails = (petId) => {
+  fetch(`https://openapi.programming-hero.com/api/peddy/pet/${petId}`)
+    .then((res) => res.json())
+    .then((data) => displayDetails(data.petData));
+}
+
+// adopt
+const loadAdopt= () =>{
+  const adoptContainer =document.getElementById("adopt-content")
+  adoptContainer.classList =""
+  document.getElementById("showAdoptData").click();
+}
+
+// load Liked Pet
+const loadLikedPet =(petId) => {
+  fetch(`https://openapi.programming-hero.com/api/peddy/pet/${petId}`)
+  .then((res) => res.json())
+  .then((data) => displayLikedPet(data.petData));
+}
+
+// display Liked Pet
+const displayLikedPet = (petData) => {
+  const likedContainer = document.getElementById("liked-pet")
+  
+  const div = document.createElement("div")
+  likedContainer.innerHTML = `
+  <div class="w-24 h-24">
+  <img
+      src=${petData.image}
+      class="h-full w-full object-cover "
+      alt="Shoes" />
+  </div>
+  `;
+  
+  likedContainer.appendChild(div);
+}
+
+
+
+// display details modal
+const displayDetails = (petData) => {
+  const detailContainer = document.getElementById("modal-content")
+
+  detailContainer.innerHTML = `
+      <div class=" border rounded-md p-4">
+         <figure class="h-[200px] rounded-md ">
+    <img
+      src=${petData.image}
+      class="h-full w-full object-cover "
+      alt="Shoes" />
+  </figure >
+  <div class=" border-b pb-3">
+    <h1 class="text-xl font-bold pt-3">${petData.pet_name}</>
+    <p class="text-sm text-[#131313B3] font-normal pt-1"> <i class="fa-solid fa-table"></i> Breed: ${petData.breed}</p>
+    <p class="text-sm text-[#131313B3] font-normal pt-1"> <i class="fa-solid fa-calendar-days"></i> Birth: ${petData.date_of_birth}</p>
+    <p class="text-sm text-[#131313B3] font-normal pt-1"> <i class="fa-solid fa-venus"></i> Gender: ${petData.gender}</p>
+    <p class="text-sm text-[#131313B3] font-normal pt-1"> <i class="fa-solid fa-tags"></i> Price: ${petData.price}$</p>
+  </div>
+  <div>
+  <h2 class="text-xl font-bold">Details Information</h2>
+  <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. 
+  The point of using is that it has a more-or-less normal distribution of letters, as opposed to using.</p>
+  </div>
+    `;
+
+  document.getElementById("showModalData").click();
+}
+
+
+// display pets
+const displayPets = (pets) => {
+  const petContainer = document.getElementById("pets")
+  petContainer.innerHTML = "";
+
+  if (pets.length == 0) {
+    petContainer.classList.remove("grid")
+    petContainer.innerHTML = `
+         <div class="min-h-[300px] flex flex-col gap-5 justify-center items-center text-center">
+         <img src="assets/error.webp"/>
+         <h1 class="text-3xl font-bold"> No Information Available </h1>
+         <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at <br> 
+          its layout. The point of using Lorem Ipsum is that it has a.</p>
+         </div>
+        `;
+    return;
+  }
+  else {
+    petContainer.classList.add("grid")
+  }
+
+  pets.forEach((pet) => {
+    console.log(pet);
+
+    const card = document.createElement("div");
+    card.classList = "card card-compact"
+    card.innerHTML = `
+        <div class=" border rounded-md p-4">
+         <figure class="h-[200px] rounded-md ">
+    <img
+      src=${pet.image}
+      class="h-full w-full object-cover "
+      alt="Shoes" />
+  </figure >
+  <div class=" border-b pb-3">
+    <h1 class="text-xl font-bold pt-3">${pet.pet_name}</>
+    <p class="text-sm text-[#131313B3] font-normal pt-1"> <i class="fa-solid fa-table"></i> Breed: ${pet.breed}</p>
+    <p class="text-sm text-[#131313B3] font-normal pt-1"> <i class="fa-solid fa-calendar-days"></i> Birth: ${pet.date_of_birth}</p>
+    <p class="text-sm text-[#131313B3] font-normal pt-1"> <i class="fa-solid fa-venus"></i> Gender: ${pet.gender}</p>
+    <p class="text-sm text-[#131313B3] font-normal pt-1"> <i class="fa-solid fa-tags"></i> Price: ${pet.price}$</p>
+  </div>
+    <div class="card-actions pt-3 flex justify-around">
+      <button onclick="loadLikedPet(${pet.petId})" class="btn bg-white hover:bg-green-400"><img src="https://img.icons8.com/?size=24&id=u8MTpAq972MG&format=png"></button>
+      <button onclick="loadAdopt()" class="btn bg-white text-[#0E7A81] ">Adopt</button>
+      <button onclick="loadDetails(${pet.petId})" class="btn bg-white text-[#0E7A81] ">Details</button>
+  </div>
+  <div> 
+    `;
+    petContainer.append(card)
+  });
+};
+
+
+// display Catagories
+const displayCatagories = (categories) => {
+  const categoryContainer = document.getElementById("categories")
+  categories.forEach((item) => {
+    console.log(item)
+
+    // button
+    const buttonContainer = document.createElement("div");
+    // button.classList = "btn";
+    buttonContainer.innerHTML = `
+        <button id="btn-${item.category}" onclick ="loadCategoryPets('${item.category}')" class="btn remove-btn bg-white lg:px-12 hover:bg-gray-200">
+             <img class="h-8 w-8" src="${item.category_icon}"/> <h4 class="text-l font-bold">${item.category} </h4>
+             </button>
+        `;
+
+    // Append button
+    categoryContainer.append(buttonContainer)
+  });
+}
+
+loadCatagories()
+loadPets();
